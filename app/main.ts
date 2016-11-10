d7a061059483527cfed91e255cbc7f07653016d5
nikita <kaidash.nikita93@gmail.com>
2016-11-10 15:15:57 +0200

    first

---
+++ app/main.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by nikita on 10.11.16.
+ */
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
